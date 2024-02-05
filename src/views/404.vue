3bafbb86326999d503ed7e08da69b18abe0e931d
caixue <caixue@233gmail.com>
2024-02-05 13:00:17 +0800

    添加翻译

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,7 @@
 <template>
-    <el-result icon="error" title="404" sub-title="对不起，您要查看的页面不存在！">
+    <el-result icon="error" title="404" :sub-title="$t('404-title')">
         <template #extra>
-            <router-link to="/"><el-button type="primary">返回首页</el-button></router-link>
+            <router-link to="/"><el-button type="primary">{{ $t('Back-main') }}</el-button></router-link>
         </template>
     </el-result>
 </template>
