c91f6ed23f30459c83d8cdf51050cd3ff3edb870
caixue <caixue@233gmail.com>
2024-04-10 11:10:20 +0800

    调整样式

--- src/views/config/shellConfig.vue
+++ src/views/config/shellConfig.vue
@@ -145,7 +145,7 @@
                                 <template v-if="base.type === 2">
                                     <el-divider v-if="index > 0" />
                                     <el-form-item :label="t('script-type')">
-                                        <el-select v-model="item.type" :placeholder="t('default-cmd')">
+                                        <el-select v-model="item.type" :placeholder="t('default-cmd')" style="width: 200px;">
                                             <el-option label="powershell" value="powershell">powershell</el-option>
                                             <el-option label="bat" value="bat">bat</el-option>
                                             <el-option label="native" value="native">native</el-option>
