f85ea91d8000a386e6e59741992d6c694b49cae9
caixue <caixue@233gmail.com>
2024-05-24 15:00:17 +0800

    添加仓库地址显示

--- src/layouts/desk.vue
+++ src/layouts/desk.vue
@@ -6,10 +6,11 @@
                     <div>{{ t('title') }}</div>
                     <div class="extra">
                         <el-button v-if="win.close" @click="onCancelColose" :icon="Remove">{{ t('cancel_close_windows')
-                        }}</el-button>
+                            }}</el-button>
                         <el-dropdown @command="onChangeLocal">
-                            <i class="el-icon el-tooltip__trigger" style="font-size: 24px;">
-                                <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.2em" height="1.2em">
+                            <i class="el-icon" style="font-size: 24px;">
+                                <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.2em"
+                                    height="1.2em">
                                     <path fill="currentColor"
                                         d="m18.5 10l4.4 11h-2.155l-1.201-3h-4.09l-1.199 3h-2.154L16.5 10h2zM10 2v2h6v2h-1.968a18.222 18.222 0 0 1-3.62 6.301a14.864 14.864 0 0 0 2.336 1.707l-.751 1.878A17.015 17.015 0 0 1 9 13.725a16.676 16.676 0 0 1-6.201 3.548l-.536-1.929a14.7 14.7 0 0 0 5.327-3.042A18.078 18.078 0 0 1 4.767 8h2.24A16.032 16.032 0 0 0 9 10.877a16.165 16.165 0 0 0 2.91-4.876L2 6V4h6V2h2zm7.5 10.885L16.253 16h2.492L17.5 12.885z">
                                     </path>
@@ -22,6 +23,13 @@
                                 </el-dropdown-menu>
                             </template>
                         </el-dropdown>
+                        <i class="el-icon" style="font-size:24px;margin: 0 10px;cursor: pointer;" @click="openGithub">
+                            <svg preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.2em" height="1.2em">
+                                <path fill="currentColor"
+                                    d="M12 2C6.475 2 2 6.475 2 12a9.994 9.994 0 0 0 6.838 9.488c.5.087.687-.213.687-.476c0-.237-.013-1.024-.013-1.862c-2.512.463-3.162-.612-3.362-1.175c-.113-.288-.6-1.175-1.025-1.413c-.35-.187-.85-.65-.013-.662c.788-.013 1.35.725 1.538 1.025c.9 1.512 2.338 1.087 2.912.825c.088-.65.35-1.087.638-1.337c-2.225-.25-4.55-1.113-4.55-4.938c0-1.088.387-1.987 1.025-2.688c-.1-.25-.45-1.275.1-2.65c0 0 .837-.262 2.75 1.026a9.28 9.28 0 0 1 2.5-.338c.85 0 1.7.112 2.5.337c1.912-1.3 2.75-1.024 2.75-1.024c.55 1.375.2 2.4.1 2.65c.637.7 1.025 1.587 1.025 2.687c0 3.838-2.337 4.688-4.562 4.938c.362.312.675.912.675 1.85c0 1.337-.013 2.412-.013 2.75c0 .262.188.574.688.474A10.016 10.016 0 0 0 22 12c0-5.525-4.475-10-10-10z">
+                                </path>
+                            </svg>
+                        </i>
                         <el-button type="primary" @click="openWin" :icon="Plus">{{ t('new_window') }}</el-button>
                     </div>
                 </div>
@@ -139,6 +147,10 @@ function onChangeLocal(command?: string) {
     electronAPI.switchLocale(locale.value);
 }
 onChangeLocal(localStorage.locale);
+
+function openGithub() {
+    electronAPI.openExternal('https://github.com/cx690/mini-shell');
+}
 </script>
 
 <style scoped lang="less">
